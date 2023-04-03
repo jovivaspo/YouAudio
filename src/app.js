fc287acb3792f9ede1ee723c24c0f331be066e64
Jorge Vivas <jovivaspo@gmail.com>
2023-04-03 14:45:25 +0200

    New origin cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set("port", config.PORT || 8001);
 //MIDDLEWARE
 app.use(
   cors({
-    origin: ["http://localhost:8000", "https://you-audio.vercel.app"],
+    origin: ["http://localhost:8000", "https://you-audio.vercel.app", "you-audio-git-main-jovivaspo.vercel.app","you-audio-2tmucbq7i-jovivaspo.vercel.app"],
   })
 );
 
